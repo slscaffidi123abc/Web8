9e7f80caa2c4b3a70cc921233279a61dcd72f102
Sandra Scaffidi <54339234+slscaffidi123abc@users.noreply.github.com>
2019-10-29 07:53:28 -0500

    Add files via upload

---
+++ ADMP.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication8.Data
+{
+    public class ADMP
+    {
+        [key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ADMIN_ID { get; set; }
+    }
+}
